3489b4bdae66874e325adfd7e6900f8f97dce4d0
Fernandez Diego <diegodavid712@gmail.com>
2024-11-20 05:36:36 -0300

    agregando Spring

---
+++ src/main/java/com/unla/utilizandoSpring/UtilizandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unla.utilizandoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilizandoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/utilizandoSpring/UtilizandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.utilizandoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
