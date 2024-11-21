1e474b47516232bf960ad7a2aee401db40039ba5
Fernandez Diego <diegodavid712@gmail.com>
2024-11-21 08:01:10 -0300

    borrado de imports en utilizandoSpringApplication,peticion GET con parametros

--- src/main/java/com/unla/utilizandoSpring/UtilizandoSpringApplication.java
+++ src/main/java/com/unla/utilizandoSpring/UtilizandoSpringApplication.java
@@ -2,10 +2,8 @@ package com.unla.utilizandoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class UtilizandoSpringApplication {
 
